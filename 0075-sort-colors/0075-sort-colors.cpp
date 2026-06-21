2b8d5b2005e62158905637d9396dbb002eaa37d0
Nishit Aggarwal <98461210+contact2nishit@users.noreply.github.com>
2026-06-20 21:40:51 -0500

    Time: 0 ms (100%), Space: 11.6 MB (78.16%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        unordered_map<int, int> freqMap;
+        for (const auto& i : nums) {
+            ++freqMap[i];
+        }
+        nums.clear();
+        nums.insert(nums.end(), freqMap[0], 0);
+        nums.insert(nums.end(), freqMap[1], 1);
+        nums.insert(nums.end(), freqMap[2], 2);
+    }
+};
